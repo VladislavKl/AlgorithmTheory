d5276018484aaec4dc71a583a8d746e0a82468f3
VladislavKl <vladkld13@gmail.com>
2019-02-22 23:50:13 +0300

    Add files via upload

---
+++ Walking 25/cmake-build-debug/CMakeFiles/Walking_25.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Walking_25.dir/main.cpp.o"
+  "Walking_25.pdb"
+  "Walking_25"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Walking_25.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
