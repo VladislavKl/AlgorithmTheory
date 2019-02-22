fc22f4ed0887fabffd9c50066cbf6f5147bd306f
VladislavKl <vladkld13@gmail.com>
2019-02-22 23:57:03 +0300

    Add files via upload

---
+++ 30Pyatnashki/cmake-build-debug/CMakeFiles/30Pyatnashki.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/30Pyatnashki.dir/main.cpp.o"
+  "30Pyatnashki.pdb"
+  "30Pyatnashki"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/30Pyatnashki.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
